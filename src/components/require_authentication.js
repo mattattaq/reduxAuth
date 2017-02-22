14750b9cee0e8fe84db3e0dcec781a57716fe224
Matthew Allbright <matthew.allbright@rooksecurity.com>
2017-02-22 15:43:22 -0500

    authorization using react router. Still needs a level that connects to a database and processes login actions.

---
+++ src/components/require_authentication.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/')
+      }
+    }
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      //console.log('Rendering', ComposedComponent); // => resourceList
+      //console.log(this.props.authenticated); //state of logged in
+      console.log(this.context); //figure out the route
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+  return connect(mapStateToProps)(Authentication);
+}
+
+//is user authenticated?
